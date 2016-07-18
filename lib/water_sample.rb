7edaf40f1bb7eebd8e7c6536b462266e687bcc40
Bernardo Galindo <bernardo466@gmail.com>
2016-07-17 19:22:11 -0500

    implement to_hash method

--- lib/water_sample.rb
+++ lib/water_sample.rb
@@ -2,7 +2,7 @@ require './lib/client/mysql_adapter'
 
 class WaterSample 
 
-  attr_accessor :site, :chloroform, :bromoform, :bromodichloromethane, :dibromichloromethane
+  attr_accessor :id, :site, :chloroform, :bromoform, :bromodichloromethane, :dibromichloromethane
 
   def self.find(sample_id)
     sample_id = sample_id.to_i
@@ -12,11 +12,21 @@ class WaterSample
   end
 
   def initialize(water_sample_values = {})
+    @id   = water_sample_values.fetch(:id, nil)
     @site = water_sample_values.fetch(:site, nil)
     @chloroform = water_sample_values.fetch(:chloroform, nil)
     @bromoform = water_sample_values.fetch(:bromoform, nil)
     @bromodichloromethane = water_sample_values.fetch(:bromodichloromethane, nil)
     @dibromichloromethane = water_sample_values.fetch(:dibromichloromethane, nil)
+  end
+
+  def to_hash(include_factors = false)
+    result = {}
+    instance_variables.each do |name|
+      name = name.to_s.gsub('@','')
+      result[name.to_sym] =  self.send(name.to_sym) 
+    end
+    result
   end
 
   private
--- spec/water_sample_spec.rb
+++ spec/water_sample_spec.rb
@@ -26,4 +26,30 @@ describe 'WaterSample' do
       expect(water_sample.dibromichloromethane).to eq(0.0109)
     end
   end
+
+  describe 'to_hash' do
+    subject(:water_sample) { WaterSample.find(2) }
+    let(:hash_water_sample) do
+      {
+        id: 2,
+        site: 'North Hollywood Pump Station (well blend)',
+        chloroform: 0.00291,
+        bromoform: 0.00487,
+        bromodichloromethane: 0.00547,
+        dibromichloromethane: 0.0109
+      }
+    end
+
+    let(:with_factor_weights) do
+     hash_water_sample.merge( {'factor_5':  0.0213, 'factor_6': 0.432, 'factor_9':  0.0321} )
+    end
+
+    it 'should respond to to_hash method' do
+      expect(water_sample).to respond_to(:to_hash)
+    end
+
+    it 'should return a hash of attributes' do
+      expect(water_sample.to_hash).to eq(hash_water_sample)
+    end
+  end
 end
